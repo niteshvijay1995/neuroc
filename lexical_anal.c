7c24af832e51923768ed89a9c824cc5774d98852
Nitesh Vijay <niteshvijay1995@gmail.com>
2016-02-15 10:39:50 +0530

    Lexical analyser module

--- lexical_anal.c
+++ lexical_anal.c
@@ -1,5 +1,22 @@
 #include<stdio.h>
-int main()
+int main(int argc, char *argv[])
 {
+	if(argc < 2)
+		printf("You must pass a file\n");
+	else if(argc >2)
+		printf("Too many files passed\n");
+	else
+	{
+		FILE *prog = fopen(argv[1], "r");
+		if(prog == 0)
+		{
+			printf("There is some problem opening the file \n");
+			exit(0); 
+		}
+		else
+		{
+			
+		}
+	}
 	printf("Hello World\n");
 }
