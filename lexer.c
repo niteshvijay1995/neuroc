62a11cb7e8aad3cf438ddcbd0a44273e813a483a
Nitesh Vijay <niteshvijay1995@gmail.com>
2016-03-01 20:23:38 +0530

    lexer upgraded

--- lexer.c
+++ lexer.c
@@ -19,10 +19,18 @@ FILE* getStream(FILE *fp, char** Buffer, size_t buffersize)
 }
 
 
-void lexime_detected(char* lexime)			//function called whenever a lexime is detected (after detecting delimiter)
+void lexeme_detected(char* lexeme)			//function called whenever a lexeme is detected (after detecting delimiter)
 {
-	printf("Lexime = %s ",lexime);
-	printf("Token = %s\n",getNextToken(lexime,states));
+	
+	char token[20];
+	strcpy(token,getNextToken(lexeme,states));
+	if(strcmp(token,"TK_ID")==0 && strlen(lexeme)>20)
+			printf("Identifier length exceeded (Limit : 20)\n"); 
+	else
+	{
+		printf("Lexeme = %s ",lexeme);
+		printf("Token = %s\n",token);
+	}	
 	fflush(stdout);
 }
 void main()
@@ -32,26 +40,27 @@ void main()
 	char ch;			//used when iterating on Buffer
 	size_t bsize;
 	bsize = 10000;			//Size of buffer
-	int i,j=0,temp;			// j -> index for lexime string | i-> index for Buffer string | temp -> index for temporary check
-	char lexime[20];		//to store lexime when reading from Buffer
+	int i,j=0,temp;			// j -> index for lexeme string | i-> index for Buffer string | temp -> index for temporary check
+	char lexeme[60];		//to store lexeme when reading from Buffer
 	int comment_flag = 0;		//1 if comment started (when character read is %) and set to 0 when '\n' is read
 	int lineno = 1;			//incremented when '\n' is read
-	FILE *fp = fopen("./testcases/testcase2.txt","r");
+	FILE *fp = fopen("./testcases/testcase4.txt","r");
 	while((fp=getStream(fp,&Buffer,bsize))!=NULL)
 	{
 		i =0;
 		//printf("%s\n",Buffer);
 		while((ch=Buffer[i])!='\0')
 		{
+			
 			if(comment_flag == 0 || ch == '\n')
 			{
 				switch(ch)
 				{
 					case ' ':
-						lexime[j] = '\0';
-						if(j!=0)		// to check that lexime is not empty					
-							lexime_detected(lexime);
-						//printf("Lexime = %s",lexime);
+						lexeme[j] = '\0';
+						if(j!=0)		// to check that lexeme is not empty					
+							lexeme_detected(lexeme);
+						//printf("lexeme = %s",lexeme);
 						j = 0;				
 						break;
 					case '%':
@@ -60,98 +69,108 @@ void main()
 						break;
 					case '\n':
 						comment_flag = 0;
-						lexime[j] = '\0';
+						lexeme[j] = '\0';
 						if(j!=0)
 							//printf("j = %d",j);
-							lexime_detected(lexime);
+							lexeme_detected(lexeme);
 						j = 0;
 						lineno++;
 						break;
 					case '\r':
 						break;
 					case ':':
-						lexime[j] = '\0';
+						lexeme[j] = '\0';
 						if(j!=0)						
-							lexime_detected(lexime);
-						lexime_detected(":");
+							lexeme_detected(lexeme);
+						lexeme_detected(":");
 						j = 0;
 						break;
 					case '[':
-						lexime[j] = '\0';
+						lexeme[j] = '\0';
 						if(j!=0)
-							lexime_detected(lexime);
-						lexime_detected("[");
+							lexeme_detected(lexeme);
+						lexeme_detected("[");
 						j = 0;
 						break;
 					case ']':
-						lexime[j] = '\0';
+						lexeme[j] = '\0';
 						if(j!=0)
-							lexime_detected(lexime);
-						lexime_detected("]");
+							lexeme_detected(lexeme);
+						lexeme_detected("]");
 						j = 0;
 						break;
 					case ';':
-						lexime[j] = '\0';
+						lexeme[j] = '\0';
 						if(j!=0)
-							lexime_detected(lexime);
-						lexime_detected(";");
+							lexeme_detected(lexeme);
+						lexeme_detected(";");
 						j = 0;
 						break;
 					case '+':
-						lexime[j] = '\0';
+						lexeme[j] = '\0';
 						if(j!=0)
-							lexime_detected(lexime);
-						lexime_detected("+");
+							lexeme_detected(lexeme);
+						lexeme_detected("+");
 						j = 0;
 						break;
 					case '-':
-						lexime[j] = '\0';
+						lexeme[j] = '\0';
 						if(j!=0)
-							lexime_detected(lexime);
-						lexime_detected("-");
+							lexeme_detected(lexeme);
+						lexeme_detected("-");
 						j = 0;
 						break;
 					case '*':
-						lexime[j] = '\0';
+						lexeme[j] = '\0';
 						if(j!=0)
-							lexime_detected(lexime);
-						lexime_detected("*");
+							lexeme_detected(lexeme);
+						lexeme_detected("*");
 						j = 0;
 						break;
 					case '/':
-						lexime[j] = '\0';
+						lexeme[j] = '\0';
 						if(j!=0)
-							lexime_detected(lexime);
-						lexime_detected("/");
+							lexeme_detected(lexeme);
+						lexeme_detected("/");
+						j = 0;
+						break;
+					case ',':
+						lexeme[j] = '\0';
+						if(j!=0)
+							lexeme_detected(lexeme);
+						lexeme_detected(",");
 						j = 0;
 						break;
 					case '=':			//for detecting ==
 						temp = i;
 						if(Buffer[++temp] == '=')
 						{
-							lexime[j] = '\0';
+							lexeme[j] = '\0';
 							if(j!=0)
-								lexime_detected(lexime);
-							lexime_detected("==");
+								lexeme_detected(lexeme);
+							lexeme_detected("==");
 							j = 0;
 							i = temp;
 							break;
 						}
+						lexeme_detected("=");
+						j = 0;
+						break;
 					case '~':
-						lexime[j] = '\0';
+						lexeme[j] = '\0';
 						if(j!=0)
-							lexime_detected(lexime);
-						lexime_detected("~");
+							lexeme_detected(lexeme);
+						lexeme_detected("~");
 						j = 0;
 						break;
 					case '!':
 						temp = i;			//for detectinn !=
 						if(Buffer[++temp] == '=')
 						{
-							lexime[j] = '\0';
+							lexeme[j] = '\0';
 							if(j!=0)
-								lexime_detected(lexime);
-							lexime_detected("!=");
+								lexeme_detected(lexeme);
+							lexeme_detected("!=");
 							j = 0;
 							i = temp;
 							break;
@@ -162,10 +181,10 @@ void main()
 						if(Buffer[++temp] == '@')
 							if(Buffer[++temp] == '@')						
 								{
-									lexime[j] = '\0';
+									lexeme[j] = '\0';
 									if(j!=0)
-										lexime_detected(lexime);
-									lexime_detected("@@@");
+										lexeme_detected(lexeme);
+									lexeme_detected("@@@");
 									j = 0;
 									i = temp;
 									break;
@@ -176,10 +195,10 @@ void main()
 						if(Buffer[++temp] == '&')
 							if(Buffer[++temp] == '&')						
 								{
-									lexime[j] = '\0';
+									lexeme[j] = '\0';
 									if(j!=0)
-										lexime_detected(lexime);
-									lexime_detected("&&&");
+										lexeme_detected(lexeme);
+									lexeme_detected("&&&");
 									j = 0;
 									i = temp;
 									break;
@@ -188,31 +207,31 @@ void main()
 					case '.':
 						temp = i;
 						/*This is to differentiate between a real number(TK_REAL) and Record type definition 
-						Here we are checking next character and if it is a digit then it should be a real number and 							hence skipping the lexime_detected call*/ 
+						Here we are checking next character and if it is a digit then it should be a real number and 							hence skipping the lexeme_detected call*/ 
 						if(isdigit(Buffer[++temp]))
 						{	
-							lexime[j] = ch;
+							lexeme[j] = ch;
 							j++;
 							break;//no need for i = temp as we no not delimiting here (loop will continue as it is)
 						}	
-						lexime[j] = '\0';
+						lexeme[j] = '\0';
 						if(j!=0)
-							lexime_detected(lexime);
-						lexime_detected(".");
+							lexeme_detected(lexeme);
+						lexeme_detected(".");
 						j = 0;
 						break;
 					case '(':
-						lexime[j] = '\0';
+						lexeme[j] = '\0';
 						if(j!=0)
-							lexime_detected(lexime);
-						lexime_detected("(");
+							lexeme_detected(lexeme);
+						lexeme_detected("(");
 						j = 0;
 						break;
 					case ')':
-						lexime[j] = '\0';
+						lexeme[j] = '\0';
 						if(j!=0)
-							lexime_detected(lexime);
-						lexime_detected(")");
+							lexeme_detected(lexeme);
+						lexeme_detected(")");
 						j = 0;
 						break;
 						break;
@@ -224,50 +243,51 @@ void main()
 							if(Buffer[++temp]=='-')
 								if(Buffer[++temp]=='-')
 									{
-										lexime[j] = '\0';
+										lexeme[j] = '\0';
 										if(j!=0)
-											lexime_detected(lexime);
-										lexime_detected("<---");
+											lexeme_detected(lexeme);
+										lexeme_detected("<---");
 										i = temp;
 										j = 0;
 										break;
 									}
 						else if(Buffer[temp] == '=')				//check for <= (less than or equals to)
 						{
-							lexime[j] = '\0';
+							lexeme[j] = '\0';
 							if(j!=0)
-								lexime_detected(lexime);
-							lexime_detected("<=");
+								lexeme_detected(lexeme);
+							lexeme_detected("<=");
 							i = temp;
 							j = 0;
 							break;	
 						}
-						lexime[j] = '\0';					//for < (less than) as delimiter
+						lexeme[j] = '\0';					//for < (less than) as delimiter
 						if(j!=0)
-							lexime_detected(lexime);
-						lexime_detected("<");
+							lexeme_detected(lexeme);
+						lexeme_detected("<");
 						j = 0;
 						break;
 					case '>':
 						if(Buffer[++temp] == '=')			//check for >= (greater than or equal to)
 						{
-							lexime[j] = '\0';
+							lexeme[j] = '\0';
 							if(j!=0)
-								lexime_detected(lexime);
-							lexime_detected(">=");
+								lexeme_detected(lexeme);
+							lexeme_detected(">=");
 							i = temp;
 							j = 0;
 							break;	
 						}
-						lexime[j] = '\0';				//for >(greater than) as delimiter
+						lexeme[j] = '\0';				//for >(greater than) as delimiter
 						if(j!=0)
-							lexime_detected(lexime);
-						lexime_detected(">");
+							lexeme_detected(lexeme);
+						lexeme_detected(">");
 						j = 0;
 						break;
 					default:
-						lexime[j] = ch;
-						j++;
+						lexeme[j] = ch;
+						if(j<60)
+							j++;
 					//	printf("d exec =  %d |",ch);
 					
 				}		//end switch
